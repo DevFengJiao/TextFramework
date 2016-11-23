753a587f1fbf3bfe33b39bd63cc2a23cca57a4f9
DevFeng <fjkvscy@sina.com>
2016-11-23 11:48:06 +0800

    Initial Commit

---
+++ TextFramework/TextFramework.h
@@ -0,0 +1,19 @@
+//
+//  TextFramework.h
+//  TextFramework
+//
+//  Created by hzyx01 on 16/11/23.
+//  Copyright © 2016年 hzyx01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TextFramework.
+FOUNDATION_EXPORT double TextFrameworkVersionNumber;
+
+//! Project version string for TextFramework.
+FOUNDATION_EXPORT const unsigned char TextFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TextFramework/PublicHeader.h>
+
+
